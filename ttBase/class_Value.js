bcf0406251ffa6da43c3ce6d6c5ba225fc6dd917
Gary Eades <gary.eades@inmechasol.com>
2026-09-14 12:53:12 -0400

    updates

--- ttBase/class_Value.js
+++ ttBase/class_Value.js
@@ -6,7 +6,7 @@
 // This is the "value" class definition.  Values have units to give physical meaning.
 // Values have serialization and logging features.
 // Values have graphical input and output features.
-class class_Value {
+class Valueclass {
 
     constructor(InitialValue = 0.0, UnitsReference = null){
         this.value = InitialValue;
