a663120e1709ec23b99d0dc734a5aaa1cc3e6472
Gary Eades <gary.eades@inmechasol.com>
2026-09-16 15:22:01 -0400

    changes

--- SimpleModelExplorer/main.js
+++ SimpleModelExplorer/main.js
@@ -13,6 +13,4 @@ var GarysGearShaft = new ideal_GearShaft("Gary's Ideal Gear Shaft", ModelExeSys)
 var SholandasGearShaft = new ideal_GearShaft("Sholanda's Ideal Gear Shaft", ModelExeSys);
 
 // Start GUI Timer
-const uGUITimer = ModelExeSys.GUITimer;
-const bGUITimer = uGUITimer.bind(ModelExeSys);
-let guiTimer = setInterval(bGUITimer(), 100);
+let guiTimer = setInterval(ModelExeSys.GUITimer.bind(ModelExeSys), 100);
