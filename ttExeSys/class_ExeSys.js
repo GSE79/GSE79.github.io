6e0814618e4c4c1d4630b1117c1662f9b18b072d
Gary Eades <gary.eades@inmechasol.com>
2026-09-16 14:37:42 -0400

    changes

--- ttExeSys/class_ExeSys.js
+++ ttExeSys/class_ExeSys.js
@@ -153,7 +153,7 @@ class modelExecutionSystem {
         {
             if(this.activeModel != null)
             {
-                this.headerActiveModel.innerHTML = this.activeModel.modelType + ": '"+this.activeModel.instanceName+"' (click to de-select)";
+                this.headerActiveModel.innerHTML = this.activeModel.modelType.value + ": '"+this.activeModel.instanceName.value+"' (click to de-select)";
             }
             else
             {
